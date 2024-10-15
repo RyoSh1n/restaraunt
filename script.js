7664781fa2cc69d7cecd267e9a432b70a4170c0b
RyoSh1n <damirsocseti@gmail.com>
2024-10-16 01:53:06 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function scrollMenu(direction) {
-    const menu = document.querySelector('.menu');
-    const scrollAmount = 300; // Количество пикселей для прокрутки
-    menu.scrollBy({
-        left: direction * scrollAmount,
-        behavior: 'smooth'
-    });
-}
